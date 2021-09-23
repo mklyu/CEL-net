187a1a95deb47127ac683a176dc19e11e18bb576
Mikel <mikelk96@gmail.com>
2021-09-23 16:05:49 +0300

    remove redundant function

--- lfp_undistort.py
+++ lfp_undistort.py
@@ -12,9 +12,6 @@ class LFPUndistort:
         self.cam = db.find_cameras("Sony", "ILCE-5100")[0]
         self.lens = db.find_lenses(self.cam, "Sony", "E PZ 16-50mm f/3.5-5.6 OSS")[0]
 
-    def GetImageName(self, imagePath):
-        return imagePath.split("/")[-1][0:-3]
-
     def ConvertImage(
         self, image, focalLength: float, fNumber: float, focusDistance: float,
     ):
